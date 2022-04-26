1d0b0e370c6cb351af2f72077ccf940e57c46323
Mat Schaffer <mat@elastic.co>
2022-04-26 11:20:20 +0900

    Trying otel metrics otlp grpc

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,20 +2,27 @@ import express from 'express';
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
 import { MeterProvider } from '@opentelemetry/sdk-metrics-base';
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { MetricExporter } from '@opentelemetry/sdk-metrics-base/build/src/export/types';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 // Create a new express application instance
 const app: express.Application = express();
 
-const { endpoint, port } = PrometheusExporter.DEFAULT_OPTIONS;
-
-const exporter = new PrometheusExporter({}, () => {
-  console.log(`prometheus scrape endpoint: http://localhost:${port}${endpoint}`);
-});
+function getExporter(): MetricExporter {
+  if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
+    return new OTLPMetricExporter();
+  } else {
+    const { endpoint, port } = PrometheusExporter.DEFAULT_OPTIONS;
+    return new PrometheusExporter({}, () => {
+      console.log(`prometheus scrape endpoint: http://localhost:${port}${endpoint}`);
+    });
+  }
+}
 
 const provider = new MeterProvider({
-  exporter,
+  exporter: getExporter(),
   interval: 2000,
 });
 
@@ -27,9 +34,10 @@ meter.createObservableGauge(
     description: 'Example of an async observable gauge with callback',
   },
   async (observableResult) => {
-    const value = await getAsyncValue();
-    observableResult.observe(value, { core: '1' });
-    observableResult.observe(value, { core: '2' });
+    const value1 = await getAsyncValue();
+    observableResult.observe(value1, { core: '1' });
+    const value2 = await getAsyncValue();
+    observableResult.observe(value2, { core: '2' });
   }
 );
 
