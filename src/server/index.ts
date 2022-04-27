1ec902e3b360e4b72d5fd55ed4842c95d9a6f5bd
Mat Schaffer <mat@elastic.co>
2022-04-28 00:17:55 +0900

    Trying to get the otel-js master API working

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,50 +1,43 @@
 import express from 'express';
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
 import { MeterProvider } from '@opentelemetry/sdk-metrics-base';
-import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
-import { MetricExporter } from '@opentelemetry/sdk-metrics-base/build/src/export/types';
-import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
 
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+// function getExporter(): MetricReader {
+//   if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
+//     return new OTLPMetricExporter();
+//   } else {
+//     const { endpoint, port } = PrometheusExporter.DEFAULT_OPTIONS;
+//     return new PrometheusExporter({}, () => {
+//       console.log(`prometheus scrape endpoint: http://localhost:${port}${endpoint}`);
+//     });
+//   }
+// }
 
-function getExporter(): MetricExporter {
-  if (process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
-    return new OTLPMetricExporter();
-  } else {
-    const { endpoint, port } = PrometheusExporter.DEFAULT_OPTIONS;
-    return new PrometheusExporter({}, () => {
-      console.log(`prometheus scrape endpoint: http://localhost:${port}${endpoint}`);
-    });
-  }
-}
-
-const provider = new MeterProvider({
-  exporter: getExporter(),
-  interval: 2000,
-});
+const provider = new MeterProvider();
+provider.addMetricReader(new PrometheusExporter());
 
 const meter = provider.getMeter('example-meter');
 
-meter.createObservableGauge(
-  'cpu_core_usage',
-  {
-    description: 'Example of an async observable gauge with callback',
-  },
-  async (observableResult) => {
-    const value1 = await getAsyncValue();
-    observableResult.observe(value1, { core: '1' });
-    const value2 = await getAsyncValue();
-    observableResult.observe(value2, { core: '2' });
-  }
-);
-
-function getAsyncValue(): Promise<number> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(Math.random());
-    }, 100);
-  });
-}
+// meter.createObservableGauge(
+//   'cpu_core_usage',
+//   async (observableResult) => {
+//     const value1 = await getAsyncValue();
+//     observableResult.observe(value1, { core: '1' });
+//     const value2 = await getAsyncValue();
+//     observableResult.observe(value2, { core: '2' });
+//   },
+//   {
+//     description: 'Example of an async observable gauge with callback',
+//   }
+// );
+//
+// function getAsyncValue(): Promise<number> {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(Math.random());
+//     }, 100);
+//   });
+// }
 
 // Create a new express application instance
 const app: express.Application = express();
